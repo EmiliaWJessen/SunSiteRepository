976c33597af79fb1ffaf65d852212c1cb0a8bc83
Emilia Elzbieta Warchulska-Jessen <emil38a7@easv365.dk>
2017-11-07 16:20:37 +0100

    SunSite

---
+++ SunsiteProject/Client.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using NntpClientLib;
+using System.IO;
+using System.Collections.ObjectModel;
+
+namespace SunsiteProject
+{
+    class Client
+    {
+        //Rfc977NntpClientWithExtensions test = new Rfc977NntpClientWithExtensions();
+
+        private const int SERVPORT = 119;
+        public string SERVHOST { get; set; } = "news.sunsite.dk";
+
+        public string userName { get; set; } = "nikosxfx@gmail.com";
+        public string userPassword { get; set; } = "9ed347";
+
+        private string OK = "200";
+
+        private TcpClient client = null;
+        private StreamReader ns = null;
+        private StreamWriter sw = null;
+        private NetworkStream networkStream = null;
+
+        private string responseMessage = "";
+        public string enter = Char.ConvertFromUtf32(13) + Char.ConvertFromUtf32(10);
+        private ObservableCollection<string> groupsList = new ObservableCollection<string>();
+        private ObservableCollection<string> articleList = new ObservableCollection<string>();
+        private ObservableCollection<string> articles = new ObservableCollection<string>();
+
+        public Client()
+        {
+            //test.Connect(SERVHOST, SERVPORT);
+            //connect();
+
+        }
+
+        public ObservableCollection<string> GroupList
+        {
+            get { return groupsList; }
+            set { groupsList = value; }
+        }
+
+        public ObservableCollection<string> ArticleList
+        {
+            get { return articleList; }
+            set { articleList = value; }
+        }
+
+        public ObservableCollection<string> Articles
+        {
+            get { return articles; }
+            set { articles = value; }
+        }
+
+        public string connect()
+        {
+            string returnCode = "";
+            try
+            {
+                client = new TcpClient(SERVHOST, SERVPORT);
+                ns = new StreamReader(client.GetStream());
+                returnCode = checkResponse(returnCode = readMessage());
+                Console.WriteLine(returnCode);
+                return returnCode;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return "";
+        }
+
+        public void logIn()
+        {
+            if (connect().Equals(OK))
+            {
+                //readMessage();
+                sendMessage("AUTHINFO user " + userName + Char.ConvertFromUtf32(13) + Char.ConvertFromUtf32(10));
+                readMessage();
+                sendMessage("AUTHINFO PASS " + userPassword + Char.ConvertFromUtf32(13) + Char.ConvertFromUtf32(10));
+                readMessage();
+                sendMessage("list " + enter);
+                readStreamLine();
+            }
+            else
+            {
+                MessageBox.Show("Try again");
+            }
+        }
+
+        public void readStreamLine()
+        {
+            string line;
+            while (!(line = ns.ReadLine()).StartsWith("."))
+            {
+                GroupList.Add(line);
+            }
+            Console.WriteLine(GroupList.Count);
+        }
+
+        public void readListArticles()
+        {
+            string line;
+            while (!(line = ns.ReadLine()).StartsWith("."))
+            {
+                ArticleList.Add(line);
+            }
+            Console.WriteLine(ArticleList.Count);
+        }
+
+        public void readArticle()
+        {
+            string line;
+            while (!(line = ns.ReadLine()).StartsWith("."))
+            {
+                Articles.Add(line);
+            }
+            Console.WriteLine(Articles.Count);
+        }
+
+        public void readStream()
+        {
+            networkStream = client.GetStream();
+
+            if (networkStream.CanRead)
+            {
+                byte[] myReadBuffer = new byte[1024];
+                StringBuilder myCompleteMessage = new StringBuilder();
+                int numberOfBytesRead = 0;
+
+                // Incoming message may be larger than the buffer size.
+                do
+                {
+                    numberOfBytesRead = networkStream.Read(myReadBuffer, 0, myReadBuffer.Length);
+
+                    myCompleteMessage.AppendFormat("{0}", Encoding.ASCII.GetString(myReadBuffer, 0, numberOfBytesRead));
+
+                }
+                while (networkStream.DataAvailable);
+
+                // Print out the received message to the console.
+                Console.WriteLine("You received the following message : " +
+                                             myCompleteMessage);
+            }
+            else
+            {
+                Console.WriteLine("Sorry.  You cannot read from this NetworkStream.");
+            }
+        }
+
+        public string readMessage()
+        {
+            responseMessage = ns.ReadLine();
+            Console.WriteLine("the message from server is: " + responseMessage);
+            return responseMessage;
+        }
+
+        public void readMessageList()
+        {
+            responseMessage = ns.ReadLine();
+            Console.WriteLine("the message from server is: " + responseMessage);
+        }
+
+        public void logIN()
+        {
+            sendMessage(writeMessage("200"));
+            sendMessage(writeMessage("381"));
+        }
+
+        public void sendMessage(string message)
+        {
+            try
+            {
+                Console.WriteLine("Connected to server......");
+
+                // Send message no 1 to the server 
+                sw = new StreamWriter(client.GetStream());
+                sw.Write(message);
+                sw.Flush();
+                Console.WriteLine("Message sent: " + message);
+                Console.Read();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public string checkResponse(string message)
+        {
+            string[] returnCode = responseMessage.Split();
+
+            message = returnCode[0];
+
+            return message;
+        }
+
+        public string writeMessage(string code)
+        {
+            string message = "";
+            switch (code)
+            {
+                case "200":
+                    return message = "AUTHINFO user " + userName + Char.ConvertFromUtf32(13) + Char.ConvertFromUtf32(10);
+                case "381":
+                    return message = "AUTHINFO PASS " + userPassword + Char.ConvertFromUtf32(13) + Char.ConvertFromUtf32(10);
+            }
+            return message;
+        }
+
+        public void disconnect()
+        {
+            if (ns != null)
+            {
+                ns.Close();
+            }
+            if (client != null)
+            {
+                client.Close();
+            }
+        }
+    }
+}
---
+++ SunsiteProject/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SunsiteProject
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int selectedGroup = -1;
+        private int selectedArticle = -1;
+        private Client client;
+        public MainWindow()
+        {
+            InitializeComponent();
+            client = new Client();
+            this.DataContext = client;
+        }
+
+        private void groupsList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            selectedGroup = groupsList.SelectedIndex;
+            Console.WriteLine(groupsList.SelectedIndex);
+        }
+
+        private void chooseButton_Click(object sender, RoutedEventArgs e)
+        {
+            string [] message = client.GroupList[selectedGroup].Split();
+            client.sendMessage("group " + message[0] + client.enter);
+            client.readMessage();
+            client.sendMessage("xover " + client.enter);
+            client.readListArticles();
+        }
+
+        private void readArticleButton_Click(object sender, RoutedEventArgs e)
+        {
+            string[] message = client.ArticleList[selectedArticle].Split();
+            client.sendMessage("article " + message[0] + client.enter);
+            client.readArticle();
+        }
+
+        private void articlesView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            selectedArticle = articlesView.SelectedIndex;
+            Console.WriteLine(selectedArticle);
+        }
+
+        private void connectButton_Click(object sender, RoutedEventArgs e)
+        {
+            WaitWindow loadingWindow = new WaitWindow();
+            try
+            {
+                loadingWindow.Show();
+                client.logIn();
+                loadingWindow.Close();
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Exception : " + ex.Message);
+            }
+            //client.logIn();
+            /*
+            client.readMessage();
+            client.sendMessage("AUTHINFO user " + client.userName + Char.ConvertFromUtf32(13) + Char.ConvertFromUtf32(10));
+            client.readMessage();
+            client.sendMessage("AUTHINFO PASS " + client.password + Char.ConvertFromUtf32(13) + Char.ConvertFromUtf32(10));
+            client.readMessage();
+            client.sendMessage("list " + client.enter);
+            client.readStreamLine();
+            */
+        }
+    }
+}
